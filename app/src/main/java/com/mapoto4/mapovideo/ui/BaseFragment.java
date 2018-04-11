5b656633daf1809eefef6e03fec084b047138f66
mapoto4 <mapoto4@gmail.com>
2018-04-11 15:58:35 +0800

    add side-menu & find sth wrong ,need think how to solve

--- app/src/main/java/com/mapoto4/mapovideo/ui/BaseFragment.java
+++ app/src/main/java/com/mapoto4/mapovideo/ui/BaseFragment.java
@@ -1,8 +1,10 @@
 package com.mapoto4.mapovideo.ui;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by mapot on 2018/4/10.
  */
 
-public class BaseFragment {
+public class BaseFragment extends Fragment {
 }
---
+++ app/src/main/java/com/mapoto4/mapovideo/ui/main/MainFragment.java
@@ -0,0 +1,89 @@
+package com.mapoto4.mapovideo.ui.main;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.mapoto4.mapovideo.R;
+import com.mapoto4.mapovideo.ui.BaseFragment;
+
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+
+/**
+ * Created by mapot on 2018/4/11.
+ */
+
+public class MainFragment extends Fragment implements ScreenShotable {
+    public static final String CLOSE = "Close";
+    public static final String BUILDING = "Building";
+    public static final String BOOK = "Book";
+    public static final String PAINT = "Paint";
+    public static final String SHOP = "Shop";
+    public static final String MOVIE = "Movie";
+
+    private View containerView;
+    protected ImageView mImageView;
+    protected int res;
+    private Bitmap bitmap;
+
+    public static MainFragment newInstance(int resId) {
+        MainFragment contentFragment = new MainFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(Integer.class.getName(), resId);
+        contentFragment.setArguments(bundle);
+        return contentFragment;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.containerView = view.findViewById(R.id.container);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        res = getArguments().getInt(Integer.class.getName());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mImageView = (ImageView) rootView.findViewById(R.id.image_content);
+        mImageView.setClickable(true);
+        mImageView.setFocusable(true);
+        mImageView.setImageResource(res);
+        return rootView;
+    }
+
+    @Override
+    public void takeScreenShot() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Bitmap bitmap = Bitmap.createBitmap(containerView.getWidth(),
+                        containerView.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                containerView.draw(canvas);
+                MainFragment.this.bitmap = bitmap;
+            }
+        };
+
+        thread.start();
+
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+}
+
